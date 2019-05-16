6251df2621f3f81cf0e603bf006258bb9ece2464
github <1023418250@qq.com>
2019-05-16 16:21:00 +0800

    test测试

---
+++ src/cn/itcast/HelloEclipse.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class HelloEclipse {
+
+	public static void main(String[] args) {
+    //main方法
+		System.out.println("Hello,eclipse");
+		System.out.println("Hello,svn");
+	}
+}
